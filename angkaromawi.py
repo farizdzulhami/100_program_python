994a3041797bfae54adf66de79c18557d1cc3583
= <=>
2024-11-08 08:02:05 +0700

    first commit

---
+++ angkaromawi.py
@@ -0,0 +1,33 @@
+# Angka romawi
+# created by Panca
+# 30-10-2024
+
+print('\n')
+print('='*25)
+print('Angka romawi')
+print('='*25)
+
+romawi = int(input('\nMasukan bilangan bulat untuk menentukan angka romawi : '))
+
+if romawi == 1 :
+    print('I')
+elif romawi == 2 :
+    print('II')
+elif romawi == 3 :
+    print('III')
+elif romawi == 4 :
+    print('IV')
+elif romawi == 5 :
+    print('V')
+elif romawi == 6 :
+    print('VI')
+elif romawi == 7 :
+    print('VII')
+elif romawi == 8 :
+    print('VIII')
+elif romawi == 9 :
+    print('IX')
+elif romawi == 10 :
+    print('X')
+else :
+    print('Angka lanjutan ada di file "angka_romawi2"\n')
