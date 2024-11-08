aa538a25c1f36703113cca20625b22bca8df71f1
= <=>
2024-11-08 08:13:44 +0700

    first commit

---
+++ lampulalulintas.py
@@ -0,0 +1,10 @@
+warna = input("Masukan warna lampu lalu lintas : ")
+
+if warna == "merah" :
+    print("berhenti")
+elif warna == "kuning" :
+    print("bersiap-siap")
+elif warna == "hijau" :
+    print("jalan")
+else:
+    print("bukan warna lampu lalu lintas")
