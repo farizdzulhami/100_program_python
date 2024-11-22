47c6c85b30023c46eb33a7107d72370a891633af
= <=>
2024-11-22 19:08:23 +0700

    first commit

---
+++ segienamberaturan.py
@@ -0,0 +1,7 @@
+print('='*40)
+print('Program Keliling SegiEnam Beraturan')
+print('='*40)
+
+s=int(input('masukan panjang sisi : '))
+keliling= 6*s
+print(f"keliling SegiEnam Beraturan adalah ={keliling} cm")
