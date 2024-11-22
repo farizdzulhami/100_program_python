5d079e492eae4172bd38e2ff39f189a250260c26
= <=>
2024-11-22 18:56:02 +0700

    first commit

---
+++ tabelperkalian.py
@@ -0,0 +1,388 @@
+print('\n')
+print('\t\t\t','='*30)
+print('\t\t\t\tTABEL PERKALIAN')
+print('\t\t\t','='*30)
+print('\n')
+
+perkalian = int(input('Masukan jumlah perkalian : '))
+
+if perkalian == 1:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+
+elif perkalian == 2:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+
+elif perkalian == 3:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+
+elif perkalian == 4:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+
+elif perkalian == 5:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+elif perkalian == 6:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+    print(end='\n')
+    print(6, end='\t')
+    angka = 6
+    while angka < 61:
+        print(angka, end='\t')
+        angka = angka + 6
+
+elif perkalian == 7:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+    print(end='\n')
+    print(6, end='\t')
+    angka = 6
+    while angka < 61:
+        print(angka, end='\t')
+        angka = angka + 6
+    print(end='\n')
+    print(7, end='\t')
+    angka = 7
+    while angka < 71:
+        print(angka, end='\t')
+        angka = angka + 7
+
+elif perkalian == 8:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+    print(end='\n')
+    print(6, end='\t')
+    angka = 6
+    while angka < 61:
+        print(angka, end='\t')
+        angka = angka + 6
+    print(end='\n')
+    print(7, end='\t')
+    angka = 7
+    while angka < 71:
+        print(angka, end='\t')
+        angka = angka + 7
+    print(end='\n')
+    print(8, end='\t')
+    angka = 8
+    while angka < 81:
+        print(angka, end='\t')
+        angka = angka + 8
+
+elif perkalian == 9:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+    print(end='\n')
+    print(6, end='\t')
+    angka = 6
+    while angka < 61:
+        print(angka, end='\t')
+        angka = angka + 6
+    print(end='\n')
+    print(7, end='\t')
+    angka = 7
+    while angka < 71:
+        print(angka, end='\t')
+        angka = angka + 7
+    print(end='\n')
+    print(8, end='\t')
+    angka = 8
+    while angka < 81:
+        print(angka, end='\t')
+        angka = angka + 8
+    print(end='\n')
+    print(9, end='\t')
+    angka = 9
+    while angka < 91:
+        print(angka, end='\t')
+        angka = angka + 9
+
+elif perkalian == 10:
+    print('*', end='\t')
+    angka = 1
+    while angka < 11:
+        print(angka, end='\t')
+        angka = angka + 1
+    print(end='\n')
+    print(1, end='\t')
+    for angka in range(1,11,1):
+        print(angka, end='\t')
+    print(end='\n')
+    print(2, end='\t')
+    angka = 2
+    while angka < 21:
+        print(angka, end='\t')
+        angka = angka + 2
+    print(end='\n')
+    print(3, end='\t')
+    angka = 3
+    while angka < 31:
+        print(angka, end='\t')
+        angka = angka + 3
+    print(end='\n')
+    print(4, end='\t')
+    angka = 4
+    while angka < 41:
+        print(angka, end='\t')
+        angka = angka + 4
+    print(end='\n')
+    print(5, end='\t')
+    angka = 5
+    while angka < 51:
+        print(angka, end='\t')
+        angka = angka + 5
+    print(end='\n')
+    print(6, end='\t')
+    angka = 6
+    while angka < 61:
+        print(angka, end='\t')
+        angka = angka + 6
+    print(end='\n')
+    print(7, end='\t')
+    angka = 7
+    while angka < 71:
+        print(angka, end='\t')
+        angka = angka + 7
+    print(end='\n')
+    print(8, end='\t')
+    angka = 8
+    while angka < 81:
+        print(angka, end='\t')
+        angka = angka + 8
+    print(end='\n')
+    print(9, end='\t')
+    angka = 9
+    while angka < 91:
+        print(angka, end='\t')
+        angka = angka + 9
+    print(end='\n')
+    print(10, end='\t')
+    angka = 10
+    while angka < 101:
+        print(angka, end='\t')
+        angka = angka + 10
+else:
+    print('Perkalian belum tersedia')
