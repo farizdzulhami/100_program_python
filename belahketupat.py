269a0a0de9b6f35f6ae93e21c3f9e2a511df4dff
= <=>
2024-11-22 19:01:39 +0700

    first commit

---
+++ belahketupat.py
@@ -0,0 +1,10 @@
+print('='*40)
+print('Program Belah Ketupat')
+print('='*40)
+
+d1 = int(input("masukan panjang dioganal pertama dalam: "))
+d2= int(input("masukan panjang dioganal kedua dalam: "))
+
+luas= 1/2 *d1 *d2
+
+print(f"luas permukaan Belah Ketupat adalah ={luas}")
