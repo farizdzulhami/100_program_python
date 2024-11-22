1855b32b81753bce2d64ee55ae3e69e7c1bbcbf6
= <=>
2024-11-22 19:09:21 +0700

    first commit

---
+++ segilimaberaturan.py
@@ -0,0 +1,7 @@
+print('='*40)
+print('Program Keliling SegiLima Beraturan')
+print('='*40)
+
+s=int(input('masukan panjang sisi : '))
+keliling= 5*s
+print(f"keliling SegiLima Beraturan adalah ={keliling} cm")
