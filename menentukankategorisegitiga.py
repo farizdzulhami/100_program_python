3b69cc4820efa6b3b48519223f5d4970ea04ae7c
= <=>
2024-11-23 17:17:25 +0700

    first commit

---
+++ menentukankategorisegitiga.py
@@ -0,0 +1,9 @@
+print(30*"\033[92m=")
+print("PROGRAM MENENTUKAN KATEGORI SEGITIGA")
+print(30*"\033[92m=")
+
+print("dengan syarat berikut\n1. a+b>c\n2.a+c>b\n3.b+c>a\njika tidak sesuai maka ini bukan segitiga !!!")
+print()
+a = float(input("masukan sisi a = "))
+b = float(input("masukan sisi b = "))
+c = float(input("masukan sisi terpanjang c = "))
