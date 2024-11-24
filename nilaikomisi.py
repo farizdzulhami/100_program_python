9c4a78efb45ef1e7fea8a8262b00a0f00008d592
= <=>
2024-11-24 09:30:17 +0700

    first commit

---
+++ nilaikomisi.py
@@ -0,0 +1,6 @@
+nama = input("masukan nama salesman: ")
+nilai_jual = int(input("masukan nilai jual: "))
+komisi = int(0.05 * nilai_jual)
+
+print (f'''salesman {nama}, Nilai penjualan yang di capai Rp.{nilai_jual}
+dan menerima komisi sebesar= Rp.{komisi}''')
